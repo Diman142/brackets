cacce784da421ace19b0798a363ddfdf39297445
Diman142 <Diman142222@gmail.com>
2021-03-04 19:51:16 +0300

    feat: add task desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = true
+    let checkArr = str.split('')
+    let arr = []
+
+    if (str.length % 2 !== 0) {
+        return false
+    }
+
+    let bracketObj = {}
+
+    bracketsConfig.forEach(item => {
+        if (item[0] === item[1]) {
+            bracketObj['b' + item[0]] = 'a' + item[0]
+            let count = 0
+            checkArr = checkArr.map((el, index) => {
+                if (el === item[0] && count % 2 === 0) {
+                    count++
+                    return 'a' + item[0]
+                } else if (el === item[0] && count % 2 !== 0) {
+                    count++
+                    return 'b' + item[0]
+                } else {
+                    return el
+                }
+            })
+        } else {
+            bracketObj[item[1]] = item[0]
+        }
+
+    })
+
+    let closeBraketsArr = Object.keys(bracketObj)
+
+    function isClose(bracket) {
+        if (closeBraketsArr.includes(bracket)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    for (let i = 0; i < checkArr.length; i++) {
+        if (isClose(checkArr[i])) {
+            let t = arr.pop()
+            if (bracketObj[checkArr[i]] != t) {
+                res = false; break;
+            }
+        } else {
+            arr.push(checkArr[i])
+        }
+    }
+    return res
 }
+
+
+
+
